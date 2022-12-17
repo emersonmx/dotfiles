63fe539c066a3b0ef06c5175f8d9978f5391d2bb
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2022-12-17 18:09:02 -0300

    [nvim] Update setup.sh

--- dot_config/nvim/executable_setup.sh
+++ dot_config/nvim/executable_setup.sh
@@ -4,4 +4,4 @@ rm -rf $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim \
     $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-nvim +'so lua/emersonmx/packer.lua' +PackerSync
+nvim +'so $HOME/.config/nvim/lua/emersonmx/packer.lua' +PackerSync
