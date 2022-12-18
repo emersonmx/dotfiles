c86afc1f6e4d3452f60b6d1fa7f3402efe743697
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2022-12-17 23:56:15 -0300

    [nvim] Add map to toggle number lines

--- dot_config/nvim/lua/emersonmx/remap.lua
+++ dot_config/nvim/lua/emersonmx/remap.lua
@@ -8,5 +8,11 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 
+vim.keymap.set('n', '<leader>n', function ()
+    local value = vim.opt.nu:get() or vim.opt.relativenumber:get()
+    vim.opt.nu = not value
+    vim.opt.relativenumber = not value
+end)
+
 vim.keymap.set({ 'n', 'v' }, '<leader>y', [['+y]])
 vim.keymap.set('n', '<leader>Y', [['+Y]])
