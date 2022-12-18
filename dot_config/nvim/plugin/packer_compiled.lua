426cb9232f702a712807fd6cb6f30873e6e90876
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2022-12-18 13:33:43 -0300

    [nvim] Update configs

--- dot_config/nvim/plugin/packer_compiled.lua
+++ dot_config/nvim/plugin/packer_compiled.lua
@@ -219,14 +219,14 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: nvim-autopairs
-time([[Config for nvim-autopairs]], true)
-try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
-time([[Config for nvim-autopairs]], false)
 -- Config for: lf.nvim
 time([[Config for lf.nvim]], true)
 try_loadstring("\27LJ\2\n*\0\0\2\0\3\0\0056\0\0\0009\0\1\0+\1\2\0=\1\2\0K\0\1\0\rlf_netrw\6g\bvim\0", "config", "lf.nvim")
 time([[Config for lf.nvim]], false)
+-- Config for: nvim-autopairs
+time([[Config for nvim-autopairs]], true)
+try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
+time([[Config for nvim-autopairs]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
