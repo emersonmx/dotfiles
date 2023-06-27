fe66517fee912ae260bfe78d7e778a78a9df72d6
Emerson MX <emersonmx@gmail.com>
2023-06-27 14:44:31 -0300

    [nvim] Update treesitter configs

--- dot_config/nvim/after/plugin/treesitter.lua
+++ dot_config/nvim/after/plugin/treesitter.lua
@@ -1,14 +1,18 @@
 require("nvim-treesitter.configs").setup({
     -- A list of parser names, or "all"
     ensure_installed = {
+        "bash",
         "c",
+        "gitcommit",
         "go",
         "javascript",
+        "json",
         "lua",
         "python",
         "rust",
         "typescript",
         "vim",
+        "yaml",
     },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
