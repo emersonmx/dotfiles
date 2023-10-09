0e6dd54ef3ce344b40716d8fb811212747a1004d
Emerson MX <emersonmx@gmail.com>
2023-10-09 16:53:55 -0300

    [nvim] Add null ls config

---
+++ dot_config/nvim/lua/emersonmx/plugins/null_ls.lua
@@ -0,0 +1,26 @@
+return {
+    "jose-elias-alvarez/null-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        local code_actions = null_ls.builtins.code_actions
+        local formatting = null_ls.builtins.formatting
+        local diagnostics = null_ls.builtins.diagnostics
+
+        local venv_bin = ".venv/bin"
+
+        null_ls.setup({
+            sources = {
+                code_actions.gitsigns,
+
+                -- Python
+                diagnostics.ruff.with({ prefer_local = venv_bin }),
+                diagnostics.mypy.with({ prefer_local = venv_bin }),
+                formatting.ruff.with({ prefer_local = venv_bin }),
+                formatting.black.with({ prefer_local = venv_bin }),
+                formatting.prettier,
+                formatting.shfmt,
+                formatting.taplo,
+            }
+        })
+    end,
+}
