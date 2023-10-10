8fbf4313928d1a51d4f320737950025ba2a6f9c1
Emerson MX <emersonmx@gmail.com>
2023-10-10 19:36:55 -0300

    [nvim] Update lsp-zero to v3

--- dot_config/nvim/lua/emersonmx/plugins/lsp_zero.lua
+++ dot_config/nvim/lua/emersonmx/plugins/lsp_zero.lua
@@ -1,6 +1,6 @@
 return {
     "VonHeikemen/lsp-zero.nvim",
-    branch = "v1.x",
+    branch = "v3.x",
     dependencies = {
         -- LSP Support
         "neovim/nvim-lspconfig",
@@ -24,35 +24,12 @@ return {
         "L3MON4D3/LuaSnip",
     },
     config = function()
-        local lsp = require("lsp-zero")
+        local lsp_zero = require("lsp-zero")
 
-        lsp.preset("recommended")
+        lsp_zero.on_attach(function(client, bufnr)
+            lsp_zero.default_keymaps({ buffer = bufnr })
 
-        local cmp = require("cmp")
-        local cmp_mappings = lsp.defaults.cmp_mappings({
-            ["<C-Space>"] = cmp.mapping.complete(),
-        })
-
-        -- disable completion with tab
-        -- this helps with copilot setup
-
-        lsp.setup_nvim_cmp({
-            mapping = cmp_mappings
-        })
-
-        lsp.set_preferences({
-            suggest_lsp_servers = false,
-            sign_icons = {
-                error = "E",
-                warn = "W",
-                hint = "H",
-                info = "I"
-            }
-        })
-
-        lsp.on_attach(function(client, bufnr)
             local opts = { buffer = bufnr, remap = false }
-
             vim.keymap.set(
                 { "n", "v" },
                 "<leader>ca",
@@ -64,12 +41,72 @@ return {
             vim.keymap.set("n", "gs", vim.lsp.buf.workspace_symbol, opts)
         end)
 
-        lsp.nvim_workspace()
-
-        lsp.setup()
+        require("mason").setup({})
+        require("mason-lspconfig").setup({
+            ensure_installed = {},
+            handlers = {
+                lsp_zero.default_setup,
+                lua_ls = function()
+                    local lua_opts = lsp_zero.nvim_lua_ls()
+                    require("lspconfig").lua_ls.setup(lua_opts)
+                end,
+            }
+        })
+        lsp_zero.set_sign_icons({
+            error = "E",
+            warn = "W",
+            hint = "H",
+            info = "I"
+        })
 
         vim.diagnostic.config({
-            virtual_text = true,
+            virtual_text = false,
+            severity_sort = true,
+            float = {
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+        })
+
+        local cmp = require("cmp")
+        local cmp_action = lsp_zero.cmp_action()
+        local cmp_format = lsp_zero.cmp_format()
+
+        require("luasnip.loaders.from_vscode").lazy_load()
+
+        vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+        cmp.setup({
+            formatting = cmp_format,
+            preselect = "item",
+            completion = {
+                completeopt = "menu,menuone,noinsert"
+            },
+            window = {
+                documentation = cmp.config.window.bordered(),
+            },
+            sources = {
+                { name = "path" },
+                { name = "nvim_lsp" },
+                { name = "nvim_lua" },
+                { name = "buffer",                 keyword_length = 3 },
+                { name = "luasnip",                keyword_length = 1 },
+                { name = "nvim_lsp_signature_help" },
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<CR>"] = cmp.mapping.confirm({ select = false }),
+                ["<C-e>"] = cmp_action.toggle_completion(),
+                ["<Tab>"] = cmp_action.tab_complete(),
+                ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                ["<C-d>"] = cmp_action.luasnip_jump_forward(),
+                ["<C-b>"] = cmp_action.luasnip_jump_backward(),
+                ["<C-f>"] = cmp.mapping.scroll_docs(5),
+                ["<C-u>"] = cmp.mapping.scroll_docs(-5),
+            }),
         })
     end,
 }
