d83e262e51d68b20c292039554598ad54c811028
Emerson MX <emersonmx@gmail.com>
2023-10-12 08:05:39 -0300

    [nvim] Remove venv

--- dot_config/nvim/executable_setup.sh
+++ dot_config/nvim/executable_setup.sh
@@ -6,22 +6,6 @@ rm -f "$SCRIPT_DIR/lazy-lock.json"
 rm -rf "$HOME/.local/share/nvim/"
 rm -rf "$HOME/.local/state/nvim/"
 
-
-rm -rf "$SCRIPT_DIR/.venv"
-python -m venv --upgrade-deps .venv
-"$SCRIPT_DIR/.venv/bin/python" --version
-"$SCRIPT_DIR/.venv/bin/python" -m pip install \
-    black \
-    flake8 \
-    flake8-bugbear \
-    flake8-print \
-    ipdb \
-    ipython \
-    mypy \
-    pep8-naming \
-    pynvim \
-    ruff
-
 MASON_PACKAGES=(
     bash-language-server \
     black \
