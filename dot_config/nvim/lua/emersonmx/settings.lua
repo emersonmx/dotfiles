b117831a0371894eb1b2bf2db1b11dd2313986ea
Emerson MX <emersonmx@gmail.com>
2023-10-12 12:21:04 -0300

    [nvim] Use stdpath instead os.geten

--- dot_config/nvim/lua/emersonmx/settings.lua
+++ dot_config/nvim/lua/emersonmx/settings.lua
@@ -12,7 +12,7 @@ vim.opt.colorcolumn = "81"
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = vim.fn.stdpath("state") .. "/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
