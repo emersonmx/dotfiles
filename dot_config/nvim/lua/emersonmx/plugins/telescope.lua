369421378e0cf89d5a48bed21e9da1aa3a5c8090
Emerson MX <emersonmx@gmail.com>
2023-12-13 14:48:17 -0300

    [nvim] Add <leader>tt to show telescope builtin

--- dot_config/nvim/lua/emersonmx/plugins/telescope.lua
+++ dot_config/nvim/lua/emersonmx/plugins/telescope.lua
@@ -8,5 +8,6 @@ return {
         vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
         vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
         vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
+        vim.keymap.set("n", "<leader>tt", builtin.builtin, {})
     end,
 }
