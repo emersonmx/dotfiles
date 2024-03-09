c6459138e7e86c64b8ac94eff693fd32010b9948
Emerson MX <emersonmx@gmail.com>
2024-03-09 18:26:46 -0300

    [nvim] Update configs

--- dot_config/nvim/lua/emersonmx/plugins.lua
+++ dot_config/nvim/lua/emersonmx/plugins.lua
@@ -435,9 +435,12 @@ require("lazy").setup({
         config = function()
             local cmp = require("cmp")
             local luasnip = require("luasnip")
-            luasnip.config.setup({})
+            luasnip.config.setup({
+                update_events = { "TextChanged", "TextChangedI" },
+            })
 
             require("luasnip.loaders.from_vscode").lazy_load()
+            -- require("luasnip.loaders.from_lua").lazy_load()
 
             cmp.setup({
                 snippet = {
