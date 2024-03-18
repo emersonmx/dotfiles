634ef7ef2d0202f503e6647a4191e1d464b40f73
Emerson MX <emersonmx@gmail.com>
2024-03-18 11:08:16 -0300

    [nvim] Update configs

--- dot_config/nvim/lua/emersonmx/plugins/init.lua
+++ dot_config/nvim/lua/emersonmx/plugins/init.lua
@@ -13,32 +13,29 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+    "folke/zen-mode.nvim",
+    "mbbill/undotree",
     "nvim-tree/nvim-web-devicons",
-
-    "tpope/vim-unimpaired",
-    "tpope/vim-repeat",
     "tpope/vim-abolish",
     "tpope/vim-eunuch",
-
     "tpope/vim-fugitive",
+    "tpope/vim-repeat",
+    "tpope/vim-unimpaired",
 
-    "mbbill/undotree",
-    "folke/zen-mode.nvim",
-
-    require("emersonmx.plugins.comment"),
     require("emersonmx.plugins.autopairs"),
-    require("emersonmx.plugins.gitsigns"),
-    require("emersonmx.plugins.onedark"),
-    require("emersonmx.plugins.which_key"),
-    require("emersonmx.plugins.suda"),
-    require("emersonmx.plugins.navigator"),
-    require("emersonmx.plugins.telescope"),
-    require("emersonmx.plugins.lsp"),
-    require("emersonmx.plugins.conform"),
     require("emersonmx.plugins.cmp"),
-    require("emersonmx.plugins.todo_comments"),
+    require("emersonmx.plugins.comment"),
+    require("emersonmx.plugins.conform"),
+    require("emersonmx.plugins.gitsigns"),
+    require("emersonmx.plugins.lsp"),
     require("emersonmx.plugins.mini"),
+    require("emersonmx.plugins.navigator"),
+    require("emersonmx.plugins.onedark"),
+    require("emersonmx.plugins.suda"),
+    require("emersonmx.plugins.telescope"),
+    require("emersonmx.plugins.todo_comments"),
     require("emersonmx.plugins.treesitter"),
+    require("emersonmx.plugins.which_key"),
 }, {
     ui = {
         icons = {
