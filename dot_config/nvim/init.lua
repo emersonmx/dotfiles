fea02eaba1a3f8936935a35253cbb11c31d82e1a
Emerson MX <emersonmx@gmail.com>
2024-05-15 10:33:12 -0300

    [nvim] Update config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,7 +2,6 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     vim.fn.system({
         "git",
@@ -13,6 +12,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
         lazypath,
     })
 end
+
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
