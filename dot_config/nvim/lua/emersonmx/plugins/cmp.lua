3a762da976fdf7ebceaf4b988d92d2f47066526e
Emerson MX <emersonmx@gmail.com>
2024-05-17 10:50:16 -0300

    [nvim] Update cmp.lua

--- dot_config/nvim/lua/emersonmx/plugins/cmp.lua
+++ dot_config/nvim/lua/emersonmx/plugins/cmp.lua
@@ -48,12 +48,12 @@ return {
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<C-y>"] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete({}),
-                ["<C-l>"] = cmp.mapping(function()
+                ["<C-j>"] = cmp.mapping(function()
                     if luasnip.expand_or_locally_jumpable() then
                         luasnip.expand_or_jump()
                     end
                 end, { "i", "s" }),
-                ["<C-h>"] = cmp.mapping(function()
+                ["<C-k>"] = cmp.mapping(function()
                     if luasnip.locally_jumpable(-1) then
                         luasnip.jump(-1)
                     end
