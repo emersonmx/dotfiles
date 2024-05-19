f8e829a2cd46e937d8a1d3187b8ff290cd7f947e
Emerson MX <emersonmx@gmail.com>
2024-05-19 15:37:13 -0300

    [nvim] Add more snippets to rust

--- dot_config/nvim/lua/emersonmx/snippets/rust.lua
+++ dot_config/nvim/lua/emersonmx/snippets/rust.lua
@@ -1,10 +1,24 @@
 ---@diagnostic disable: undefined-global
 
 return {
+    s("derive", fmt("#[derive({})]", i(1))),
+    s(
+        "impl",
+        fmt(
+            [[
+            impl {} {{
+                {}
+            }}
+            ]],
+            { i(1, "Type"), i(0) }
+        )
+    ),
     s("option", fmt("Option<{}>", i(1))),
     s("some", fmt("Some({})", i(1))),
     s("none", t("None")),
     s("result", fmt("Result<{}, {}>", { i(1, "T"), i(2, "E") })),
     s("ok", fmt("Ok({})", i(1))),
     s("err", fmt("Err({})", i(1))),
+    s("pr", fmt("println!({})", i(1))),
+    s("fmt", fmt("format!({})", i(1))),
 }
