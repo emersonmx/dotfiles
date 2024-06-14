fac2ae5d8770efb365f091186896105737bfb9f7
Emerson MX <emersonmx@gmail.com>
2024-06-14 14:08:25 -0300

    [nvim] Add golang snippets

---
+++ dot_config/nvim/lua/emersonmx/snippets/go.lua
@@ -0,0 +1,37 @@
+---@diagnostic disable: undefined-global
+
+return {
+    s("pr", fmt("fmt.Println({})", i(0))),
+    s("prf", fmt("fmt.Printf({})", i(0))),
+    s("re", t("return")),
+    s("fs", fmt("fmt.Sprintf({})", i(0))),
+    s(
+        "fn",
+        fmt(
+            [[
+            func {}({}){} {{
+                {}
+            }}
+            ]],
+            { i(1, "funcName"), i(2, "..."), i(3), i(0) }
+        )
+    ),
+    s(
+        "m",
+        fmt(
+            [[
+            func ({} {}) {}({}){} {{
+                {}
+            }}
+            ]],
+            {
+                i(1, "receiver"),
+                i(2, "type"),
+                i(3, "methodName"),
+                i(4, "..."),
+                i(5),
+                i(0),
+            }
+        )
+    ),
+}
