704a1a1e9fc5ffd396c1ad709d37b1d8e45ff9de
Emerson MX <emersonmx@gmail.com>
2024-06-14 14:11:04 -0300

    [nvim] Update javascript snippets

--- dot_config/nvim/lua/emersonmx/snippets/javascript.lua
+++ dot_config/nvim/lua/emersonmx/snippets/javascript.lua
@@ -1,5 +1,51 @@
 ---@diagnostic disable: undefined-global
 
 return {
-    s("af", fmt("({}) => {}", { i(1), i(0, "statement") })),
+    s(
+        "cb",
+        fmt(
+            [[
+            {{
+                {}
+            }}
+            ]],
+            i(0)
+        )
+    ),
+    s("pr", fmt("console.log({})", i(0))),
+    s("re", t("return")),
+    s("af", fmt("({}) => {}", { i(1), i(0, "...") })),
+    s(
+        "if",
+        fmt(
+            [[
+            if ({}) {{
+                {}
+            }}
+            ]],
+            { i(1, "..."), i(0) }
+        )
+    ),
+    s(
+        "cls",
+        fmt(
+            [[
+            class {} {{
+                {}
+            }}
+            ]],
+            { i(1, "ClassName"), i(0) }
+        )
+    ),
+    s(
+        "ctor",
+        fmt(
+            [[
+            constructor({}) {{
+                {}
+            }}
+            ]],
+            { i(1), i(0) }
+        )
+    ),
 }
