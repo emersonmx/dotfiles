7bd45e7fa618543ba3d62173c42d5a8802256b8a
Emerson MX <emersonmx@gmail.com>
2024-06-25 13:02:16 -0300

    [nvim] Fix clipboard

--- dot_config/nvim/plugin/options.lua
+++ dot_config/nvim/plugin/options.lua
@@ -22,19 +22,6 @@ opt.undofile = true
 
 opt.showmode = false
 
-if vim.fn.has("wsl") == 1 then
-    vim.g.clipboard = {
-        copy = {
-            ["+"] = "win32yank.exe -i --crlf",
-            ["*"] = "win32yank.exe -i --crlf",
-        },
-        paste = {
-            ["+"] = "win32yank.exe -o --lf",
-            ["*"] = "win32yank.exe -o --lf",
-        },
-    }
-end
-
 opt.ignorecase = true
 opt.smartcase = true
 
