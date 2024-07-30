9c5d8952abb6e66d0d87117b92d9a8599379ede7
Emerson MX <emersonmx@gmail.com>
2024-07-30 15:00:36 -0300

    [nvim] Update go snippets

--- dot_config/nvim/lua/emersonmx/snippets/go.lua
+++ dot_config/nvim/lua/emersonmx/snippets/go.lua
@@ -2,7 +2,65 @@
 
 return {
     s("re", t("return")),
+    s("pkg", t("package")),
 
+    s(
+        "if",
+        fmt(
+            [[
+            if {} {{
+                {}
+            }}
+            ]],
+            { i(1), i(0) }
+        )
+    ),
+    s(
+        "sw",
+        fmt(
+            [[
+            switch {} {{
+            {}
+            }}
+            ]],
+            { i(1), i(0) }
+        )
+    ),
+    s("cs", fmt("case {}:", i(1))),
+    s("df", t("default:")),
+    s(
+        "st",
+        fmt(
+            [[
+            type {} struct {{
+                {}
+            }}
+            ]],
+            { i(1), i(0) }
+        )
+    ),
+    s(
+        "in",
+        fmt(
+            [[
+            type {} interface {{
+                {}
+            }}
+            ]],
+            { i(1), i(0) }
+        )
+    ),
+    s(
+        "af",
+        fmt(
+            [[
+            func({}){} {{
+                {}
+            }}
+            ]],
+            { i(1), i(2), i(0) }
+        )
+    ),
     s(
         "fn",
         fmt(
@@ -33,6 +91,19 @@ return {
         )
     ),
 
+    s("o", fmt("{} := {}", { i(1), i(0) })),
+    s("a", fmt("{} = {}", { i(1), i(0) })),
+    s(
+        "cb",
+        fmt(
+            [[
+            {{
+                {}
+            }}
+            ]],
+            i(0)
+        )
+    ),
     s("pr", fmt("fmt.Println({})", i(0))),
     s("prf", fmt("fmt.Printf({})", i(0))),
     s("fs", fmt("fmt.Sprintf({})", i(0))),
