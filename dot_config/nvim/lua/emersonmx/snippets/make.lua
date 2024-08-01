cd8f54a72b8f3c446c605d0343412899dcf64b7b
Emerson MX <emersonmx@gmail.com>
2024-08-01 13:15:37 -0300

    [nvim] Add makefile snippets

---
+++ dot_config/nvim/lua/emersonmx/snippets/make.lua
@@ -0,0 +1,15 @@
+---@diagnostic disable: undefined-global
+
+return {
+    s(
+        "tg",
+        fmt(
+            [[
+            .PHONY: {}
+            {}:{}
+            	{}
+            ]],
+            { rep(1), i(1, "target"), i(2), i(0) }
+        )
+    ),
+}
