4646ac8a909c07605ca4c55ec2ab943477de168a
Emerson MX <emersonmx@gmail.com>
2024-08-05 14:41:48 -0300

    [nvim] Update snippets

--- dot_config/nvim/lua/emersonmx/snippets/typescript.lua
+++ dot_config/nvim/lua/emersonmx/snippets/typescript.lua
@@ -5,4 +5,27 @@ return {
     s("po", t("protected ")),
     s("pi", t("private ")),
     s("ro", t("readonly ")),
+
+    s(
+        "st",
+        fmt(
+            [[
+            type {} = {{
+                {}
+            }};
+            ]],
+            { i(1), i(0) }
+        )
+    ),
+    s(
+        "in",
+        fmt(
+            [[
+            interface {} {{
+                {}
+            }}
+            ]],
+            { i(1), i(0) }
+        )
+    ),
 }
