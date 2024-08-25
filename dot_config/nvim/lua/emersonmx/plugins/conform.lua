87f4adba5f1cc4cf7a9f075a4b3d708f576392c4
Emerson MX <emersonmx@gmail.com>
2024-08-25 13:11:54 -0300

    [nvim] Add templ to conform

--- dot_config/nvim/lua/emersonmx/plugins/conform.lua
+++ dot_config/nvim/lua/emersonmx/plugins/conform.lua
@@ -12,6 +12,7 @@ return {
             css = { "prettierd", "prettier", stop_after_first = true },
             gdscript = { "gdformat" },
             go = { "goimports", "gofumpt" },
+            templ = { "templ" },
             javascript = { "prettierd", "prettier", stop_after_first = true },
             lua = { "stylua" },
             python = function(bufnr)
