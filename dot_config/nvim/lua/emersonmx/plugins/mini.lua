5cf0b8c471b90e1150b452c642f07f02b2d637f1
Emerson MX <emersonmx@gmail.com>
2025-03-07 17:05:40 -0300

    [nvim] Use mini.pairs

--- dot_config/nvim/lua/emersonmx/plugins/mini.lua
+++ dot_config/nvim/lua/emersonmx/plugins/mini.lua
@@ -2,7 +2,10 @@ return {
     "echasnovski/mini.nvim",
     config = function()
         require("mini.ai").setup({ n_lines = 500 })
+        require("mini.pairs").setup()
         require("mini.surround").setup()
+        require("mini.comment").setup()
+
         local statusline = require("mini.statusline")
         statusline.setup({ use_icons = vim.g.have_nerd_font })
         statusline.section_location = function()
