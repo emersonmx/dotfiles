60fb1c7dccd38897ca75e66c1c462ff7823c63c0
Emerson MX <emersonmx@gmail.com>
2025-04-17 12:32:15 -0300

    [nvim] Remove unused plugins

--- dot_config/nvim/lua/emersonmx/plugins/mini.lua
+++ dot_config/nvim/lua/emersonmx/plugins/mini.lua
@@ -5,6 +5,25 @@ return {
         require("mini.pairs").setup()
         require("mini.surround").setup()
         require("mini.comment").setup()
+        require("mini.diff").setup()
+        require("mini.git").setup()
+        require("mini.indentscope").setup()
+
+        local hipatterns = require("mini.hipatterns")
+        hipatterns.setup({
+            highlighters = {
+                fixme = {
+                    pattern = "%f[%w]()FIXME()%f[%W]",
+                    group = "MiniHipatternsFixme",
+                },
+                todo = {
+                    pattern = "%f[%w]()TODO()%f[%W]",
+                    group = "MiniHipatternsTodo",
+                },
+
+                hex_color = hipatterns.gen_highlighter.hex_color(),
+            },
+        })
 
         local statusline = require("mini.statusline")
         statusline.setup({ use_icons = vim.g.have_nerd_font })
