efa9e020d14ecbf53fbb232dfbab880d2d1326c3
Emerson MX <emersonmx@gmail.com>
2025-05-16 12:29:03 -0300

    [nvim] Update markdown configs

--- dot_config/nvim/lua/emersonmx/plugins/markdown.lua
+++ dot_config/nvim/lua/emersonmx/plugins/markdown.lua
@@ -2,7 +2,7 @@ return {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
-        "echasnovski/mini.nvim",
+        "nvim-tree/nvim-web-devicons",
     },
     ft = { "markdown", "codecompanion" },
     opts = {},
