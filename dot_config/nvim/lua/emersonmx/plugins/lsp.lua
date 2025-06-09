23ce0e83aa0537939a2099535aefe50aedce9bfb
Emerson MX <emersonmx@gmail.com>
2025-06-09 12:40:06 -0300

    [nvim] Update lsp and autocmds

--- dot_config/nvim/lua/emersonmx/plugins/lsp.lua
+++ dot_config/nvim/lua/emersonmx/plugins/lsp.lua
@@ -9,83 +9,8 @@ return {
         "b0o/SchemaStore.nvim",
     },
     config = function()
-        vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup(
-                "custom-lsp-attach",
-                { clear = true }
-            ),
-            callback = function(event)
-                local map = function(keys, func, desc)
-                    vim.keymap.set(
-                        "n",
-                        keys,
-                        func,
-                        { buffer = event.buf, desc = "LSP: " .. desc }
-                    )
-                end
-                local tb = require("telescope.builtin")
-
-                map("gd", tb.lsp_definitions, "Goto Definition")
-                map("gr", tb.lsp_references, "Goto References")
-                map("gI", tb.lsp_implementations, "Goto Implementation")
-                map("<leader>D", tb.lsp_type_definitions, "Type Definition")
-                map("<leader>ds", tb.lsp_document_symbols, "Document Symbols")
-                map(
-                    "<leader>ws",
-                    tb.lsp_dynamic_workspace_symbols,
-                    "Workspace Symbols"
-                )
-                map("<leader>rn", vim.lsp.buf.rename, "Rename")
-                map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
-                map("K", function()
-                    vim.lsp.buf.hover({ wrap = false, max_width = 80 })
-                end, "Hover Documentation")
-                map("gD", vim.lsp.buf.declaration, "Goto Declaration")
-                local client = vim.lsp.get_client_by_id(event.data.client_id)
-                if
-                    client
-                    and client.server_capabilities.documentHighlightProvider
-                then
-                    vim.api.nvim_create_autocmd(
-                        { "CursorHold", "CursorHoldI" },
-                        {
-                            buffer = event.buf,
-                            callback = vim.lsp.buf.document_highlight,
-                        }
-                    )
-
-                    vim.api.nvim_create_autocmd(
-                        { "CursorMoved", "CursorMovedI" },
-                        {
-                            buffer = event.buf,
-                            callback = vim.lsp.buf.clear_references,
-                        }
-                    )
-                end
-            end,
-        })
-
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities = vim.tbl_deep_extend(
-            "force",
-            capabilities,
-            require("cmp_nvim_lsp").default_capabilities()
-        )
-
-        local languages = {
-            python = { require("efmls-configs.linters.mypy") },
-        }
-        local efmls_config = {
-            filetypes = vim.tbl_keys(languages),
-            settings = {
-                rootMarkers = { ".git/" },
-                languages = languages,
-            },
-            init_options = {
-                documentFormatting = true,
-                documentRangeFormatting = true,
-            },
-        }
+        require("mason").setup()
+        require("mason-lspconfig").setup()
 
         local servers = {
             bashls = {},
@@ -93,9 +18,22 @@ return {
             cmake = {},
             docker_compose_language_service = {},
             dockerls = {},
-            efm = efmls_config,
+            efm = {
+                filetypes = { "python" },
+                settings = {
+                    rootMarkers = { ".git/" },
+                    languages = {
+                        python = { require("efmls-configs.linters.mypy") },
+                    },
+                },
+                init_options = {
+                    documentFormatting = true,
+                    documentRangeFormatting = true,
+                },
+            },
             emmet_language_server = {},
             eslint = {},
+            gdscript = { manual_install = true },
             golangci_lint_ls = {},
             gopls = {},
             html = {},
@@ -113,12 +51,17 @@ return {
             lua_ls = {
                 settings = {
                     Lua = {
-                        runtime = { version = "LuaJIT" },
+                        runtime = {
+                            version = "LuaJIT",
+                            path = {
+                                "lua/?.lua",
+                                "lua/?/init.lua",
+                            },
+                        },
                         workspace = {
                             checkThirdParty = false,
                             library = {
-                                "${3rd}/luv/library",
-                                unpack(vim.api.nvim_get_runtime_file("", true)),
+                                vim.env.VIMRUNTIME,
                             },
                         },
                         completion = {
@@ -156,10 +99,7 @@ return {
             },
         }
 
-        require("mason").setup()
-
-        local ensure_installed = vim.tbl_keys(servers or {})
-        vim.list_extend(ensure_installed, {
+        local ensure_installed = {
             "clang-format",
             "commitlint",
             "djlint",
@@ -180,26 +120,99 @@ return {
             "taplo",
             "yamlfmt",
             "yamllint",
-        })
+        }
+        local servers_to_install = vim.tbl_filter(function(key)
+            local t = servers[key]
+            if type(t) == "table" then
+                return not t.manual_install
+            else
+                return t
+            end
+        end, vim.tbl_keys(servers))
+        vim.list_extend(ensure_installed, servers_to_install)
+
         require("mason-tool-installer").setup({
             ensure_installed = ensure_installed,
         })
 
-        require("mason-lspconfig").setup({
-            handlers = {
-                function(server_name)
-                    local server = servers[server_name] or {}
-                    server.capabilities = vim.tbl_deep_extend(
-                        "force",
-                        {},
-                        capabilities,
-                        server.capabilities or {}
-                    )
-                    vim.lsp.enable(server)
-                end,
-            },
-        })
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = vim.tbl_deep_extend(
+            "force",
+            capabilities,
+            require("cmp_nvim_lsp").default_capabilities()
+        )
 
-        vim.lsp.enable("gdscript")
+        for name, config in pairs(servers) do
+            config = vim.tbl_deep_extend(
+                "force",
+                {},
+                { capabilities = capabilities },
+                config
+            )
+            vim.lsp.config(name, config)
+            vim.lsp.enable(name)
+        end
+
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup(
+                "custom-lsp-attach",
+                { clear = true }
+            ),
+            callback = function(event)
+                local bufnr = event.buf
+                local client = assert(
+                    vim.lsp.get_client_by_id(event.data.client_id),
+                    "must have valid client"
+                )
+
+                local map = function(keys, func, desc)
+                    vim.keymap.set(
+                        "n",
+                        keys,
+                        func,
+                        { buffer = bufnr, desc = "LSP: " .. desc }
+                    )
+                end
+                local tb = require("telescope.builtin")
+
+                map("gd", tb.lsp_definitions, "Goto Definition")
+                map("gr", tb.lsp_references, "Goto References")
+                map("gI", tb.lsp_implementations, "Goto Implementation")
+                map("<leader>D", tb.lsp_type_definitions, "Type Definition")
+                map("<leader>ds", tb.lsp_document_symbols, "Document Symbols")
+                map(
+                    "<leader>ws",
+                    tb.lsp_dynamic_workspace_symbols,
+                    "Workspace Symbols"
+                )
+                map("<leader>rn", vim.lsp.buf.rename, "Rename")
+                map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
+                map("K", function()
+                    vim.lsp.buf.hover({ wrap = false, max_width = 80 })
+                end, "Hover Documentation")
+                map("gD", vim.lsp.buf.declaration, "Goto Declaration")
+
+                if
+                    client
+                    and client.server_capabilities.documentHighlightProvider
+                then
+                    vim.api.nvim_create_autocmd(
+                        { "CursorHold", "CursorHoldI" },
+                        {
+                            buffer = bufnr,
+                            callback = vim.lsp.buf.document_highlight,
+                        }
+                    )
+
+                    vim.api.nvim_create_autocmd(
+                        { "CursorMoved", "CursorMovedI" },
+                        {
+                            buffer = bufnr,
+                            callback = vim.lsp.buf.clear_references,
+                        }
+                    )
+                end
+            end,
+        })
     end,
 }
