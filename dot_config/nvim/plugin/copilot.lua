d857788eadcee498ad6faff9a725bcb2ffa3c671
Emerson MX <emersonmx@gmail.com>
2025-08-28 10:04:28 -0300

    nvim: use <C-Y> to copilot accept map

--- dot_config/nvim/plugin/copilot.lua
+++ dot_config/nvim/plugin/copilot.lua
@@ -1,6 +1,6 @@
 vim.g.copilot_no_tab_map = true
 
-vim.keymap.set("i", "<C-y>", 'copilot#Accept("\\<CR>")', {
+vim.keymap.set("i", "<C-Y>", 'copilot#Accept("\\<CR>")', {
     expr = true,
     replace_keycodes = false,
 })
