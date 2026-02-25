4eefb55b8da8e2c16f07c88624d6766485dadc96
Emerson MX <emersonmx@gmail.com>
2026-02-25 10:05:18 -0300

    nvim: disable all lang providers

--- dot_config/nvim/plugin/options.lua
+++ dot_config/nvim/plugin/options.lua
@@ -47,6 +47,8 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_list_hide = [[\(^\|\s\s\)\zs\.\S\+]]
 
-vim.g.python3_host_prog = vim.fn.stdpath("config") .. "/.venv/bin/python"
-vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python_provider = 0
+vim.g.loaded_python3_provider = 0
