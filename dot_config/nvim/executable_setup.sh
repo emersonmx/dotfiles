7cb1f6b21693271fc213f4386b41053a7e5506fc
Emerson MX <emersonmx@gmail.com>
2026-03-03 09:42:16 -0300

    nvim: update setup.sh

--- dot_config/nvim/executable_setup.sh
+++ dot_config/nvim/executable_setup.sh
@@ -6,8 +6,3 @@ rm -f "$SCRIPT_DIR/lazy-lock.json"
 rm -f "$SCRIPT_DIR/lazyvim.json"
 rm -rf "$HOME/.local/share/nvim/"
 rm -rf "$HOME/.local/state/nvim/"
-
-rm -rf "$SCRIPT_DIR/.venv"
-python -m venv --upgrade-deps .venv
-"$SCRIPT_DIR/.venv/bin/python" --version
-"$SCRIPT_DIR/.venv/bin/python" -m pip install pynvim
