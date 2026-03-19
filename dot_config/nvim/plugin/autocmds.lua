17099d0a051c24f5e8e79542e78e0cfb003cd0fd
Emerson MX <emersonmx@gmail.com>
2026-03-19 12:27:42 -0300

    nvim: update autocmds

--- dot_config/nvim/plugin/autocmds.lua
+++ dot_config/nvim/plugin/autocmds.lua
@@ -8,9 +8,23 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-    group = vim.api.nvim_create_augroup("custom-file-types", {}),
     pattern = { "*.h" },
     command = "set filetype=c",
+})
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+    pattern = { "*.h" },
+    command = "set filetype=c",
+})
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+    callback = function(args)
+        local home = vim.fn.expand("$HOME")
+        local target_dir = home .. "/.cache/zsh/completions/"
+        if vim.startswith(args.file, target_dir) then
+            vim.bo.filetype = "zsh"
+        end
+    end,
 })
 
 vim.api.nvim_create_autocmd("WinEnter", {
