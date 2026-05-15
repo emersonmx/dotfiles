89a3922c087111cb56c3c89c49a8381ed84f1df4
Emerson MX <emersonmx@gmail.com>
2026-05-15 14:17:18 -0300

    nvim: use vim.uv

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,7 +1,7 @@
 vim.opt.modeline = false
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
     local out = vim.fn.system({
         "git",
