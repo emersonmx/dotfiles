af93bb1431b756e10710ab69db8db19b500185e7
Emerson MX <emersonmx@gmail.com>
2026-05-15 17:00:43 -0300

    nvim: move codecompanion opts to config

--- dot_config/nvim/lua/emersonmx/plugins/codecompanion.lua
+++ dot_config/nvim/lua/emersonmx/plugins/codecompanion.lua
@@ -1,31 +1,48 @@
-local default_adapter = "copilot"
-
 return {
     "olimorris/codecompanion.nvim",
-    opts = {
-        ignore_warnings = true,
-        opts = {
-            language = "pt-BR",
-        },
-        strategies = {
-            chat = {
-                adapter = default_adapter,
-            },
-            inline = {
-                adapter = default_adapter,
-            },
-            cmd = {
-                adapter = default_adapter,
-            },
-        },
-        display = {
-            chat = {
-                auto_scroll = false,
-            },
-        },
-    },
     dependencies = {
         "nvim-lua/plenary.nvim",
         "zbirenbaum/copilot.lua",
     },
+    config = function()
+        local default_adapter = "copilot"
+
+        require("codecompanion").setup({
+            ignore_warnings = true,
+            opts = {
+                language = "pt-BR",
+            },
+            strategies = {
+                chat = {
+                    adapter = default_adapter,
+                },
+                inline = {
+                    adapter = default_adapter,
+                },
+                cmd = {
+                    adapter = default_adapter,
+                },
+            },
+            display = {
+                chat = {
+                    auto_scroll = false,
+                },
+            },
+        })
+
+        vim.keymap.set(
+            { "n", "v" },
+            "<leader>aa",
+            "<cmd>CodeCompanionChat Toggle<cr>",
+            { noremap = true, silent = true }
+        )
+        vim.keymap.set(
+            "v",
+            "ga",
+            "<cmd>CodeCompanionChat Add<cr>",
+            { noremap = true, silent = true }
+        )
+
+        vim.cmd([[cab cc CodeCompanion]])
+    end,
 }
