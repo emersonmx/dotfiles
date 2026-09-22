87f0c7ae9543463586fd3ec551b48c278b5619fa
Emerson MX <emersonmx@gmail.com>
2026-09-22 11:02:47 -0300

    nvim: simplify treesitter setup

--- dot_config/nvim/lua/emersonmx/plugins/treesitter.lua
+++ dot_config/nvim/lua/emersonmx/plugins/treesitter.lua
@@ -25,30 +25,13 @@ local languages = {
     "yaml",
 }
 
-local function has_query(lang, name)
-    return #vim.treesitter.query.get_files(lang, name) > 0
-end
-
-vim.api.nvim_create_user_command("TSInstallDefault", function()
-    require("nvim-treesitter").install(languages):wait(60000)
-    print("Treesitter parsers installed")
-end, {
-    desc = "Install the default set of treesitter parsers",
-})
-
 vim.api.nvim_create_autocmd("FileType", {
     pattern = languages,
-    callback = function(args)
-        local lang = args.match
-
+    callback = function()
         vim.treesitter.start()
-        if has_query(lang, "folds") then
-            vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-            vim.wo.foldmethod = "expr"
-        end
-        if has_query(lang, "indents") then
-            vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
-        end
+        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.wo.foldmethod = "expr"
+        vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
     end,
 })
 
@@ -56,4 +39,9 @@ return {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
     build = ":TSUpdate",
+    config = function()
+        local treesitter = require("nvim-treesitter")
+        treesitter.setup()
+        treesitter.install(languages):wait(60000)
+    end,
 }
